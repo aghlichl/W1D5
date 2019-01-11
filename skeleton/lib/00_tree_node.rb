1b8c49039565288b959a802c430d29ad210b1a1f
aghlichl <aurashghlichloo@gmail.com>
2019-01-11 15:05:04 -0800

    Implement Phase I and II

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,31 @@
 class PolyTreeNode
+  attr_accessor :value, :parent, :children
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  
+  def parent=(node)
+    if self.parent.nil?
+        @parent = node 
+        if node.nil?
+        else
+            unless node.children.include?(self)
+                node.children.push(self)
+            end
+        end 
+    else
+        (self.parent).children.delete(self)
+            @parent = node 
+            if node.nil?
+            else
+                unless node.children.include?(self)
+                    node.children.push(self)
+                end
+            end 
+    end
+  end 
 
 end
